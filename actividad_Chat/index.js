8109f37b67cfbd77bc107c2a812ea77a9d73eaf3
zhihao <zuiaiduola1@gmail.com>
2020-11-02 23:13:53 +0100

    informar cuando conecta un cliente y mostrar su nickname

--- actividad_Chat/index.js
+++ actividad_Chat/index.js
@@ -1,19 +1,50 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var clients = []
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+function sendMsg(msg, user)
+{
+  console.log(typeof(clients))
+  for(let client in clients)
+  {
+    if(client != user.id)
+    {
+      
+      clients[client].socket.emit('chat message', msg)
+    }
+  }
+}
+
 io.on('connection', function(socket){
-  console.log('a user connected');
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    if(clients[socket.id] != undefined){
+      console.log(clients[socket.id].name+" disconectado")
+      sendMsg(clients[socket.id].name+" disconectado",socket)
+      delete clients[socket.id]
+      
+    }
   });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    sendMsg(clients[socket.id].name+": "+ msg,socket)
+  });
+  socket.on('name', function(msg){
+    console.log(msg+ ' conectado');
+    if(clients[socket.id] != undefined)
+    {
+      let client = clients[socket.id]
+      sendMsg(client.name+ ' ha cambiado nombre por '+ msg,socket)
+      client.name = msg
+    }
+    else{
+      clients[socket.id] = {name: msg, socket:socket}
+      sendMsg(msg+ ' conectado',socket)
+    }
+     
   });
 });
 
